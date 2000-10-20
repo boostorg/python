bf3a858018ec283afb795d0c40858b1fe0e5859b
Dave Abrahams <dave@boostpro.com>
2000-10-20 01:39:40 +0000

    Workarounds for Dec Alpha compiler bug
    [SVN r8015]

--- pyconfig.h
+++ pyconfig.h
@@ -19,7 +19,7 @@
 #  define PY_INLINE_FRIEND ::py
 # endif
 
-# if !defined(__GNUC__) && !defined(__MWERKS__) && !defined(__BORLANDC__) && defined(_MSC_VER)
+# if defined(BOOST_MSVC)
 #  define PY_COMPILER_IS_MSVC 1
 #  if _MSC_VER <= 1200
 #   define PY_MSVC6_OR_EARLIER 1
@@ -27,6 +27,17 @@
 
 # pragma warning (disable : 4786)
 
+# endif
+
+// Work around the broken library implementation/strict ansi checking on some
+// EDG-based compilers (e.g. alpha), which incorrectly warn that the result of
+// offsetof() is not an integer constant expression.
+# if defined(__DECCXX_VER) && __DECCXX_VER <= 60290024
+#  define PY_OFFSETOF(s_name, s_member) \
+        ((size_t)__INTADDR__(&(((s_name *)0)->s_member)))
+# else
+#  define PY_OFFSETOF(s_name, s_member) \
+        offsetof(s_name, s_member)
 # endif
 
 // The STLport puts all of the standard 'C' library names in std (as far as the
