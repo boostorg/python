82ef4c070d9a375c0863ad3a8ab840e53d092ad2
nobody <nobody@localhost>
2001-02-07 23:58:46 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.2'.
    [SVN r9019]

--- example/tst_noncopyable.py
+++
@@ -1,8 +0,0 @@
-import noncopyable_export
-import noncopyable_import
-s1 = noncopyable_export.store(1)
-print s1.recall()
-s2 = noncopyable_export.store(2)
-print s2.recall()
-s3 = noncopyable_import.add_stores(s1, s2)
-print s3.recall()
