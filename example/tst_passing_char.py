f1d4a38121a4edbd829eb82a265356465a616b82
Ralf W. Grosse-Kunstleve <rwgk@yahoo.com>
2001-02-09 03:31:32 +0000

    Added to/from_python converters for [plain] char. Reverted to original converters for un/signed char.
    [SVN r9046]

---
+++ example/tst_passing_char.py
@@ -0,0 +1,20 @@
+import passing_char
+print passing_char.get_char()
+print passing_char.get_signed_char()
+print passing_char.get_unsigned_char()
+for arg in (-97, 97, -140, 140, "a", "ab"):
+  try:
+    print 'char', arg, ':'
+    passing_char.use_char(arg)
+  except (TypeError, ValueError), e:
+    print e.args[0]
+  try:
+    print 'signed char', arg, ':'
+    passing_char.use_signed_char(arg)
+  except (TypeError, ValueError), e:
+    print e.args[0]
+  try:
+    print 'unsigned char', arg, ':'
+    passing_char.use_unsigned_char(arg)
+  except (TypeError, ValueError), e:
+    print e.args[0]
