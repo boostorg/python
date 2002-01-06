4b88b9eed19077136414ff961ff648fcf7c9d791
Dave Abrahams <dave@boostpro.com>
2002-01-06 14:31:27 +0000

    Added most of the module_builder interface
    [SVN r12226]

--- include/boost/python/module.hpp
+++ include/boost/python/module.hpp
@@ -8,5 +8,68 @@
 
 # include <boost/python/errors.hpp>
 # include <boost/python/detail/config.hpp>
+# include <boost/python/reference.hpp>
+# include <boost/python/make_function.hpp>
+# include <boost/python/objects.hpp>
+
+namespace boost { namespace python {
+
+class BOOST_PYTHON_DECL module_base
+{
+ public:
+    // Create a module. REQUIRES: only one module is created per module.
+    module_base(const char* name);
+    ~module_base();
+
+    // Add elements to the module
+    void add(PyObject* x, const char* name);
+    void add(PyTypeObject* x, const char* name = 0);
+    void add(ref x, const char*name);
+    void add_overload(objects::function* x, const char* name);
+
+    // Return true iff a module is currently being built.
+    static bool initializing();
+    
+    // Return the name of the module currently being built.
+    // REQUIRES: initializing() == true
+    static string name();
+
+    // Return a pointer to the Python module object being built
+    PyObject* module() const;
+
+ private:
+    PyObject* m_module;
+    static PyMethodDef initial_methods[1];
+};
+
+class module : public module_base
+{
+ public:
+    module(const char* name)
+        : module_base(name) {}
+
+# if 0
+    template <class Fn>
+    void def_raw(Fn fn, const char* name)
+    {
+        add(detail::new_raw_arguments_function(fn), name);
+    }
+# endif 
+    template <class Fn>
+    void def(Fn fn, const char* name)
+    {
+        add_overload(make_function(fn), name);
+    }
+};
+
+//
+// inline implementations
+//
+inline PyObject* module_base::module() const
+{
+    return m_module;
+}
+
+}} // namespace boost::python
 
 #endif // MODULE_DWA20011221_HPP
