55a431883925c4acbd22fab58d880c59f68e2e21
Dave Abrahams <dave@boostpro.com>
2002-02-03 01:07:10 +0000

    Handle references too
    [SVN r12655]

--- include/boost/python/detail/unwind_type.hpp
+++ include/boost/python/detail/unwind_type.hpp
@@ -40,10 +40,78 @@ unwind_type_cv(U const volatile* p, const_volatile_, Generator* = 0)
     
 template <class Generator, class U>
 inline typename Generator::result_type
-unwind_type(U* p, Generator* = 0)
+unwind_ptr_type(U* p, Generator* = 0)
 {
     typedef typename cv_category<U>::type tag;
     return unwind_type_cv<Generator>(p, tag());
+}
+
+template <bool is_ptr>
+struct unwind_helper
+{
+    template <class Generator, class U>
+    static typename Generator::result_type
+    execute(U p, Generator* = 0)
+    {
+        return unwind_ptr_type(p, (Generator*)0);
+    }
+
+    template <class Generator, class U>
+    struct apply
+    {
+        static typename Generator::result_type
+        execute()
+        {
+            return unwind_ptr_type(U(0),(Generator*)0);
+        }
+    };
+};
+
+template <>
+struct unwind_helper<false>
+{
+    template <class Generator, class U>
+    static typename Generator::result_type
+    execute(U& p, Generator* = 0)
+    {
+        return unwind_ptr_type(&p, (Generator*)0);
+    }
+
+    template <class Generator, class U>
+    struct apply
+    {
+        static typename Generator::result_type
+        execute()
+        {
+            return unwind_ptr_type((U*)0,(Generator*)0);
+        }
+    };
+};
+
+template <class Generator, class U>
+inline typename Generator::result_type
+unwind_type(U& p, Generator* = 0)
+{
+    return unwind_helper<is_pointer<U>::value>::execute(p, (Generator*)0);
+}
+
+template <class Generator, class U>
+inline typename Generator::result_type
+unwind_type(U const& p, Generator* = 0)
+{
+    return unwind_helper<is_pointer<U>::value>::execute(p, (Generator*)0);
+}
+
+// Call this one with both template parameters explicitly specified
+// and no function arguments:
+//
+//      return unwind_type<my_generator,T>();
+//
+template <class Generator, class U>
+inline typename Generator::result_type
+unwind_type(type<U>*p = 0, Generator* = 0)
+{
+    return unwind_helper<is_pointer<U>::value>::template apply<Generator,U>::execute();
 }
 
 }}} // namespace boost::python::detail
