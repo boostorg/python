b303d49634423240d8a8e0190eb6d7ffcf620b4a
Dave Abrahams <dave@boostpro.com>
2002-02-15 16:59:39 +0000

    remove defunct code
    [SVN r12820]

--- include/boost/python/return_internal_reference.hpp
+++ include/boost/python/return_internal_reference.hpp
@@ -7,35 +7,10 @@
 # define RETURN_INTERNAL_REFERENCE_DWA2002131_HPP
 
 # include <boost/python/default_call_policies.hpp>
-# include <boost/type_traits/object_traits.hpp>
 # include <boost/python/reference_existing_object.hpp>
-# include <boost/python/to_python_indirect.hpp>
 # include <boost/python/with_custodian_and_ward.hpp>
 
 namespace boost { namespace python { 
-
-namespace detail
-{
-  template <class T>
-  struct return_internal_reference_requires_a_pointer_or_reference_return_type
-# if defined(__GNUC__) && __GNUC__ >= 3 || defined(__EDG__)
-  {}
-# endif
-  ;
-}
-
-struct internal_reference_to_python_generator
-{
-    template <class T>
-    struct apply
-    {
-        typedef typename mpl::select_type<
-            is_object<T>::value
-            , detail::return_internal_reference_requires_a_pointer_or_reference_return_type<T>
-            , to_python_indirect<T, detail::make_reference_holder>
-        >::type type;
-    };
-};
 
 template <std::size_t owner_arg = 1, class Base = default_call_policies>
 struct return_internal_reference
