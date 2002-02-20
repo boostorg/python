47c1c6288ccd4a92fbb6f856ddf484640897c094
Dave Abrahams <dave@boostpro.com>
2002-02-20 05:14:41 +0000

    Added error checking
    [SVN r12862]

--- include/boost/python/return_internal_reference.hpp
+++ include/boost/python/return_internal_reference.hpp
@@ -9,14 +9,32 @@
 # include <boost/python/default_call_policies.hpp>
 # include <boost/python/reference_existing_object.hpp>
 # include <boost/python/with_custodian_and_ward.hpp>
+# include <boost/mpl/select_type.hpp>
 
 namespace boost { namespace python { 
+
+namespace detail
+{
+  template <std::size_t>
+  struct return_internal_reference_owner_arg_must_be_greater_than_zero
+# if defined(__GNUC__) && __GNUC__ >= 3 || defined(__EDG__)
+  {}
+# endif
+  ;
+}
 
 template <std::size_t owner_arg = 1, class Base = default_call_policies>
 struct return_internal_reference
     : with_custodian_and_ward_postcall<0, owner_arg, Base>
 {
-    typedef reference_existing_object result_converter;
+ private:
+    BOOST_STATIC_CONSTANT(bool, legal = owner_arg > 0);
+ public:
+    typedef typename mpl::select_type<
+        legal
+        , reference_existing_object
+        , detail::return_internal_reference_owner_arg_must_be_greater_than_zero<owner_arg>
+    >::type result_converter;
 };
 
 }} // namespace boost::python
