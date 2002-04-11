7d8b6d149ead39baff11c01957d6dbe5befd53d8
Dave Abrahams <dave@boostpro.com>
2002-04-11 21:01:10 +0000

    Fixed installation/configuration checks
    [SVN r13451]

--- build/__init__.py
+++ build/__init__.py
@@ -3,12 +3,15 @@
 
 if ! $(gNO_PYTHON_INSTALL)
 {
-    ECHO Couldn't find Python $(PYTHON_VERSION) installation in $(PYTHON_ROOT) ;
+    ECHO "Couldn't find Python $(PYTHON_VERSION) installation in $(PYTHON_ROOT)" ;
     ECHO skipping Boost.Python library build ;
     ECHO You can configure the location of your python installation, by setting: ;
     ECHO PYTHON_ROOT - currently \"$(PYTHON_ROOT)\" ;
     ECHO PYTHON_VERSION - currently \"$(PYTHON_VERSION)\" ;
-    ECHO PYTHON_INCLUDES - configured from PYTHON_ROOT, currently \"$(PYTHON_INCLUDES)\" ;
-    ECHO PYTHON_LIB_PATH - configured from PYTHON_ROOT, currently \"$(PYTHON_LIB_PATH)\" ;
+    ECHO ;
+    ECHO "The following are automatically configured from PYTHON_ROOT if not otherwise set" ;
+    ECHO "    PYTHON_INCLUDES    - path to Python #include directories; currently" \"$(PYTHON_INCLUDES)\" ;
+    ECHO "    PYTHON_LIB_PATH    - path to Python library; currently" \"$(PYTHON_LIB_PATH)\" ;
+    ECHO "    PYTHON_STDLIB_PATH - path to Python standard library modules; currently" \"$(PYTHON_STDLIB_PATH)\" ;
 }
 gNO_PYTHON_INSTALL ?= true ;
