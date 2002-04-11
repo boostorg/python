4018b284e320a6005cac49fc20cad4d3e02a6489
Dave Abrahams <dave@boostpro.com>
2002-04-11 04:17:29 +0000

    returning takes policies by-pointer for GCC 2.95.3 :(
    [SVN r13441]

--- include/boost/python/preprocessed/caller.hpp
+++ include/boost/python/preprocessed/caller.hpp
@@ -9,253 +9,253 @@
 template<class P,class R>
 PyObject*operator()(R(*f)(),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0>
 PyObject*operator()(R(*f)(A0),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1>
 PyObject*operator()(R(*f)(A0,A1),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2>
 PyObject*operator()(R(*f)(A0,A1,A2),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3>
 PyObject*operator()(R(*f)(A0,A1,A2,A3),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4>
 PyObject*operator()(R(*f)(A0,A1,A2,A3,A4),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5>
 PyObject*operator()(R(*f)(A0,A1,A2,A3,A4,A5),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
 PyObject*operator()(R(*f)(A0,A1,A2,A3,A4,A5,A6),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
 PyObject*operator()(R(*f)(A0,A1,A2,A3,A4,A5,A6,A7),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
 PyObject*operator()(R(*f)(A0,A1,A2,A3,A4,A5,A6,A7,A8),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 
 template<class P,class R,class A0>
 PyObject*operator()(R(A0::*f)(),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1>
 PyObject*operator()(R(A0::*f)(A1),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2>
 PyObject*operator()(R(A0::*f)(A1,A2),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3>
 PyObject*operator()(R(A0::*f)(A1,A2,A3),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8,A9),PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0>
 PyObject*operator()(R(A0::*f)()const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1>
 PyObject*operator()(R(A0::*f)(A1)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2>
 PyObject*operator()(R(A0::*f)(A1,A2)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3>
 PyObject*operator()(R(A0::*f)(A1,A2,A3)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8,A9)const,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0>
 PyObject*operator()(R(A0::*f)()volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1>
 PyObject*operator()(R(A0::*f)(A1)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2>
 PyObject*operator()(R(A0::*f)(A1,A2)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3>
 PyObject*operator()(R(A0::*f)(A1,A2,A3)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8,A9)volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0>
 PyObject*operator()(R(A0::*f)()const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1>
 PyObject*operator()(R(A0::*f)(A1)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2>
 PyObject*operator()(R(A0::*f)(A1,A2)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3>
 PyObject*operator()(R(A0::*f)(A1,A2,A3)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 template<class P,class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
 PyObject*operator()(R(A0::*f)(A1,A2,A3,A4,A5,A6,A7,A8,A9)const volatile,PyObject*args,PyObject*keywords,P const&policies)const
 {
-    return returning<R>::call(f,args,keywords,policies);
+    return returning<R>::call(f,args,keywords,&policies);
 }
 
 #endif // CALLER_DWA2002410_HPP
