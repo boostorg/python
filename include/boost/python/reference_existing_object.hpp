97acbddb65f3f6a90651263b158f4b97efedfc09
Dave Abrahams <dave@boostpro.com>
2002-04-16 14:29:30 +0000

    Changes for MPL v2
    [SVN r13504]

--- include/boost/python/reference_existing_object.hpp
+++ include/boost/python/reference_existing_object.hpp
@@ -6,7 +6,7 @@
 #ifndef REFERENCE_EXISTING_OBJECT_DWA200222_HPP
 # define REFERENCE_EXISTING_OBJECT_DWA200222_HPP
 # include <boost/python/detail/indirect_traits.hpp>
-# include <boost/mpl/select_type.hpp>
+# include <boost/mpl/select_if.hpp>
 # include <boost/python/to_python_indirect.hpp>
 # include <boost/type_traits/composite_traits.hpp>
 
@@ -32,7 +32,7 @@ struct reference_existing_object
         BOOST_STATIC_CONSTANT(
             bool, ok = is_pointer<T>::value || is_reference<T>::value);
         
-        typedef typename mpl::select_type<
+        typedef typename mpl::select_if<
             ok
             , to_python_indirect<T, detail::make_reference_holder>
             , detail::reference_existing_object_requires_a_pointer_or_reference_return_type<T>
