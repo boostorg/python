53c69e7ad5be16a90b10f25852f63b85a463d230
Dave Abrahams <dave@boostpro.com>
2002-05-16 00:56:42 +0000

    Merged from RC_1_28_0
    [SVN r13944]

--- build/__init__.py
+++ build/__init__.py
@@ -3,15 +3,20 @@
 
 if ! $(gNO_PYTHON_INSTALL)
 {
-    ECHO "Couldn't find Python $(PYTHON_VERSION) installation in $(PYTHON_ROOT)" ;
+    ECHO "---------------------------------------------------------------------" ;
     ECHO skipping Boost.Python library build ;
     ECHO You can configure the location of your python installation, by setting: ;
-    ECHO PYTHON_ROOT - currently \"$(PYTHON_ROOT)\" ;
-    ECHO PYTHON_VERSION - currently \"$(PYTHON_VERSION)\" ;
+    ECHO "PYTHON_ROOT    - currently" \"$(PYTHON_ROOT:J=" ")\" ;
+    ECHO "PYTHON_VERSION - The 2-part python Major.Minor version number (e.g." ;
+    ECHO "                 \"2.2\", NOT \"2.2.1\")  - currently" \"$(PYTHON_VERSION)\" ;
     ECHO ;
-    ECHO "The following are automatically configured from PYTHON_ROOT if not otherwise set" ;
-    ECHO "    PYTHON_INCLUDES    - path to Python #include directories; currently" \"$(PYTHON_INCLUDES)\" ;
-    ECHO "    PYTHON_LIB_PATH    - path to Python library; currently" \"$(PYTHON_LIB_PATH)\" ;
-    ECHO "    PYTHON_STDLIB_PATH - path to Python standard library modules; currently" \"$(PYTHON_STDLIB_PATH)\" ;
+    ECHO "The following are automatically configured from PYTHON_ROOT if not" ;
+    ECHO "otherwise set:" ;
+    ECHO "    PYTHON_INCLUDES    - path to Python #include directories; currently" \"$(PYTHON_INCLUDES:J=" ")\" ;
+    ECHO "    PYTHON_LIB_PATH    - path to Python library; currently" ;
+    ECHO "                         " \"$(PYTHON_LIB_PATH:J=" ")\" ;
+    ECHO "    PYTHON_STDLIB_PATH - path to Python standard library modules; currently" ;
+    ECHO "                         " \"$(PYTHON_STDLIB_PATH:J=" ")\" ;
+    ECHO "---------------------------------------------------------------------" ;
 }
 gNO_PYTHON_INSTALL ?= true ;
