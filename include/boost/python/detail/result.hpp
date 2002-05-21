00b27c20da9e2832b76101a0c966208a8d01265b
Dave Abrahams <dave@boostpro.com>
2002-05-21 23:18:58 +0000

    Added detail/result.hpp and tests
    [SVN r14000]

---
+++ include/boost/python/detail/result.hpp
@@ -0,0 +1,88 @@
+// Copyright David Abrahams 2002. Permission to copy, use,
+// modify, sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+#ifndef RESULT_DWA2002521_HPP
+# define RESULT_DWA2002521_HPP
+
+# include <boost/python/detail/preprocessor.hpp>
+# include <boost/preprocessor/comma_if.hpp>
+# include <boost/mpl/aux_/preprocessor.hpp>
+# include <boost/preprocessor/comma_if.hpp>
+# include <boost/mpl/select_type.hpp>
+# include <boost/type.hpp>
+# include <boost/type_traits/object_traits.hpp>
+
+namespace boost { namespace python { namespace detail { 
+
+// Defines a family of overloaded function which, given x, a function
+// pointer, member [function] pointer, or an AdaptableFunction object,
+// returns a pointer to type<R>*, where R is the result type of
+// invoking the result of bind(x).
+//
+// In order to work around bugs in deficient compilers, if x might be
+// an AdaptableFunction object, you must pass OL as a second argument
+// to get this to work portably.
+
+# ifndef BOOST_PYTHON_GENERATE_CODE
+#  include <boost/python/preprocessed/result.hpp>
+# endif 
+
+#  define BOOST_PYTHON_FIRST_ARGUMENT_PF(args, ignored)                                         \
+template <class R BOOST_PP_COMMA_IF(args) BOOST_MPL_TEMPLATE_PARAMETERS(0, args, class A)>      \
+boost::type<R>* result(BOOST_PYTHON_FN(*,0,args), long = 0) { return 0; }
+
+#  define BOOST_PYTHON_FIRST_ARGUMENT_PMF(args, cv)                                     \
+template <class R, BOOST_MPL_TEMPLATE_PARAMETERS(0, args, class A)>                     \
+boost::type<R>* result(BOOST_PYTHON_FN(A0::*,1,args)cv(), long = 0) { return 0; }
+
+BOOST_PYTHON_REPEAT_ARITY_2ND(BOOST_PYTHON_FIRST_ARGUMENT_PF, nil)
+BOOST_PYTHON_REPEAT_MF_CV_2ND(BOOST_PYTHON_FIRST_ARGUMENT_PMF)
+
+template <class R, class T>
+boost::type<R>* result(R (T::*), long = 0) { return 0; }
+
+
+#  if defined(BOOST_MSVC) && BOOST_MSVC <= 1300  /* it almost works with VC6, but not VC7 */    \
+   || (defined(BOOST_MSVC) && _MSC_FULL_VER == 13012108)  /*  or the VC7.01 alpha */            \
+   || defined(__GNUC__) && __GNUC__ < 3                                                         \
+   || defined(__MWERKS__) && __MWERKS__ < 3000
+// This code actually works on all implementations, but why use it when we don't have to?
+template <class T>
+struct get_result_type
+{
+    typedef boost::type<typename T::result_type> type;
+};
+
+struct void_type
+{
+    typedef void type;
+};
+
+template <class T>
+struct result_result
+{
+    typedef typename mpl::select_type<
+        is_class<T>::value
+        , get_result_type<T>
+        , void_type
+        >::type t1;
+
+    typedef typename t1::type* type;
+};
+template <class X>
+typename result_result<X>::type
+result(X const&, int = 0) { return 0; }
+
+#  else // Simpler code for more-capable compilers
+
+template <class X>
+boost::type<typename X::result_type>*
+result(X const&, int = 0) { return 0; }
+
+#  endif 
+
+}}} // namespace boost::python::detail
+
+#endif // RESULT_DWA2002521_HPP
---
+++ include/boost/python/preprocessed/result.hpp
@@ -0,0 +1,412 @@
+// Copyright David Abrahams 2002. Permission to copy, use,
+// modify, sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+#ifndef RESULT2_DWA2002521_HPP
+# define RESULT2_DWA2002521_HPP
+
+template<class R>
+boost::type<R>*result(R(*)(),long=0)
+{
+    return 0;
+}
+template<class R,class A0>
+boost::type<R>*result(R(*)(A0),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1>
+boost::type<R>*result(R(*)(A0,A1),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2>
+boost::type<R>*result(R(*)(A0,A1,A2),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3>
+boost::type<R>*result(R(*)(A0,A1,A2,A3),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8,A9),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14>
+boost::type<R>*result(R(*)(A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14),long=0)
+{
+    return 0;
+}
+ 
+template<class R,class A0>
+boost::type<R>*result(R(A0::*)(),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1>
+boost::type<R>*result(R(A0::*)(A1),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2>
+boost::type<R>*result(R(A0::*)(A1,A2),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3>
+boost::type<R>*result(R(A0::*)(A1,A2,A3),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14),long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14,class A15>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15),long=0)
+{
+    return 0;
+}
+template<class R,class A0>
+boost::type<R>*result(R(A0::*)()const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1>
+boost::type<R>*result(R(A0::*)(A1)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2>
+boost::type<R>*result(R(A0::*)(A1,A2)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3>
+boost::type<R>*result(R(A0::*)(A1,A2,A3)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14,class A15>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15)const,long=0)
+{
+    return 0;
+}
+template<class R,class A0>
+boost::type<R>*result(R(A0::*)()volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1>
+boost::type<R>*result(R(A0::*)(A1)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2>
+boost::type<R>*result(R(A0::*)(A1,A2)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3>
+boost::type<R>*result(R(A0::*)(A1,A2,A3)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14,class A15>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15)volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0>
+boost::type<R>*result(R(A0::*)()const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1>
+boost::type<R>*result(R(A0::*)(A1)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2>
+boost::type<R>*result(R(A0::*)(A1,A2)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3>
+boost::type<R>*result(R(A0::*)(A1,A2,A3)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14)const volatile,long=0)
+{
+    return 0;
+}
+template<class R,class A0,class A1,class A2,class A3,class A4,class A5,class A6,class A7,class A8,class A9,class A10,class A11,class A12,class A13,class A14,class A15>
+boost::type<R>*result(R(A0::*)(A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15)const volatile,long=0)
+{
+    return 0;
+}
+ 
+ 
+#endif // RESULT2_DWA2002521_HPP
