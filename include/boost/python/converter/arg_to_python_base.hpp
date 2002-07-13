3375cdbb49b400906f04f3a717e61f9a1c150f80
Dave Abrahams <dave@boostpro.com>
2002-07-13 12:11:06 +0000

    Fixed for VC7.1
    [SVN r14433]

--- include/boost/python/converter/arg_to_python_base.hpp
+++ include/boost/python/converter/arg_to_python_base.hpp
@@ -14,12 +14,12 @@ namespace boost { namespace python { namespace converter {
 namespace detail
 {
   struct BOOST_PYTHON_DECL arg_to_python_base
-# if !defined(BOOST_MSVC) || _MSC_FULL_VER != 13102140
+# if !defined(BOOST_MSVC) || BOOST_MSVC <= 1300 || _MSC_FULL_VER > 13102171
       : handle<>
 # endif 
   {
       arg_to_python_base(void const volatile* source, to_python_function_t);
-# if defined(BOOST_MSVC) && _MSC_FULL_VER == 13102140
+# if defined(BOOST_MSVC) && BOOST_MSVC > 1300 && _MSC_FULL_VER <= 13102171
       PyObject* get() const { return m_ptr.get(); }
       PyObject* release() { return m_ptr.release(); }
    private:
