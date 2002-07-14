5b803f00e13f5805f67bfbad013afc1f1a122fdf
Dave Abrahams <dave@boostpro.com>
2002-07-14 12:38:41 +0000

    VC6 workarounds
    [SVN r14447]

--- test/object_manager.cpp
+++ test/object_manager.cpp
@@ -6,31 +6,27 @@
 #include <boost/python/converter/object_manager.hpp>
 #include <boost/python/borrowed.hpp>
 #include <boost/static_assert.hpp>
-#include "test_class.hpp"
 
 using namespace boost::python;
 using namespace boost::python::converter;
 
-template <class T> struct undefined;
+struct X {};
 
 int main()
 {
     BOOST_STATIC_ASSERT(is_object_manager<handle<> >::value);
     BOOST_STATIC_ASSERT(!is_object_manager<int>::value);
-    BOOST_STATIC_ASSERT(!is_object_manager<test_class<> >::value);
+    BOOST_STATIC_ASSERT(!is_object_manager<X>::value);
     
     BOOST_STATIC_ASSERT(is_reference_to_object_manager<handle<>&>::value);
     BOOST_STATIC_ASSERT(is_reference_to_object_manager<handle<> const&>::value);
     BOOST_STATIC_ASSERT(is_reference_to_object_manager<handle<> volatile&>::value);
     BOOST_STATIC_ASSERT(is_reference_to_object_manager<handle<> const volatile&>::value);
 
-//    undefined<is_reference_to_object_manager<handle<> >::t1> x1;
-//    undefined<is_reference_to_object_manager<handle<> >::t2> x2;
-    
     BOOST_STATIC_ASSERT(!is_reference_to_object_manager<handle<> >::value);
-    BOOST_STATIC_ASSERT(!is_reference_to_object_manager<test_class<> >::value);
-    BOOST_STATIC_ASSERT(!is_reference_to_object_manager<test_class<>&>::value);
-    BOOST_STATIC_ASSERT(!is_reference_to_object_manager<test_class<>const&>::value);
+    BOOST_STATIC_ASSERT(!is_reference_to_object_manager<X>::value);
+    BOOST_STATIC_ASSERT(!is_reference_to_object_manager<X&>::value);
+    BOOST_STATIC_ASSERT(!is_reference_to_object_manager<X const&>::value);
     
     return 0;
 }
