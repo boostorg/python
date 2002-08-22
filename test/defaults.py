2f89a8eb584849a5fc5fa42b5c3f875317bf8b09
Ralf W. Grosse-Kunstleve <rwgk@yahoo.com>
2002-08-22 15:23:25 +0000

    additional tests for X::foo
    [SVN r15056]

--- test/defaults.py
+++ test/defaults.py
@@ -26,6 +26,24 @@
 'int(3); char(Y); string(Hello World); double(0.0); '
 >>> x.bar(4, 'Z', "Hi There", 3.3)
 'int(4); char(Z); string(Hi There); double(3.3); '
+>>> x.foo(5)
+'int(5); bool(0); '
+>>> x.foo(6, 0)
+'int(6); bool(0); '
+>>> x.foo(7, 1)
+'int(7); bool(1); '
+>>> x.foo("A")
+'string(A); bool(0); '
+>>> x.foo("B", False)
+'string(B); bool(0); '
+>>> x.foo("C", True)
+'string(C); bool(1); '
+>>> x.foo([0,1,2], [2,3,4])
+'list([0, 1, 2]); list([2, 3, 4]); bool(0); '
+>>> x.foo([0,1,2], [2,3,4], False)
+'list([0, 1, 2]); list([2, 3, 4]); bool(0); '
+>>> x.foo([0,1,2], [2,3,4], True)
+'list([0, 1, 2]); list([2, 3, 4]); bool(1); '
 >>>
 
 
