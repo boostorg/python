d274a8395b7d02082b0470a467e8015ba9685e99
Joel de Guzman <djowel@gmail.com>
2002-09-04 22:10:37 +0000

    pardon, wrong commit. one more try. sorry.
    [SVN r15161]

--- include/boost/python/detail/defaults_gen.hpp
+++ include/boost/python/detail/defaults_gen.hpp
@@ -108,7 +108,7 @@ struct func_stubs_base {};
 #define BPL_IMPL_MEM_FUNC_WRAPPER_GEN(INDEX, DATA)                              \
     static RT BOOST_PP_CAT(func_, INDEX)                                        \
     (                                                                           \
-        ClassT& obj BOOST_PP_COMMA_IF(INDEX)                                    \
+        ClassT& obj,                                    \
         BOOST_PP_ENUM                                                           \
         (                                                                       \
             BOOST_PP_ADD(BOOST_PP_TUPLE_ELEM(3, 1, DATA), INDEX),               \
