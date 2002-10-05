e9757c46e3b90a21a3b7f383abf9028f595aa9b3
Dave Abrahams <dave@boostpro.com>
2002-10-05 19:46:26 +0000

    *** empty log message ***
    [SVN r15741]

--- include/boost/python/init.hpp
+++ include/boost/python/init.hpp
@@ -1,10 +1,10 @@
 ///////////////////////////////////////////////////////////////////////////////
 //
-// Copyright David Abrahams 2002. Permission to copy, use,
-// modify, sell and distribute this software is granted provided this
-// copyright notice appears in all copies. This software is provided
-// "as is" without express or implied warranty, and with no claim as
-// to its suitability for any purpose.
+// Copyright David Abrahams 2002, Joel de Guzman, 2002. Permission to copy,
+// use, modify, sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided "as is"
+// without express or implied warranty, and with no claim as to its
+// suitability for any purpose.
 //
 ///////////////////////////////////////////////////////////////////////////////
 #ifndef INIT_JDG20020820_HPP
