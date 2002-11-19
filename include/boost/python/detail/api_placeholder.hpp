c9af6ca94b3cf69c7c48045384e0133570c7a710
Dave Abrahams <dave@boostpro.com>
2002-11-19 17:39:17 +0000

    Add missing copyright notices
    [SVN r16329]

--- include/boost/python/detail/api_placeholder.hpp
+++ include/boost/python/detail/api_placeholder.hpp
@@ -1,3 +1,8 @@
+// Copyright David Abrahams 2002. Permission to copy, use,
+// modify, sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
 #ifndef BOOST_PYTHON_API_PLACE_HOLDER_HPP
 #define BOOST_PYTHON_API_PLACE_HOLDER_HPP
 
