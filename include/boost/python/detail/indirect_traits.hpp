257a6c45f89175974d6b29449ef5b35151f63754
Dave Abrahams <dave@boostpro.com>
2003-03-08 08:51:45 +0000

    Remove flotsam
    [SVN r17782]

--- include/boost/python/detail/indirect_traits.hpp
+++ include/boost/python/detail/indirect_traits.hpp
@@ -173,24 +173,6 @@ struct is_reference_to_pointer<T* const volatile&> : mpl::true_
 };
 
 template <class T>
-struct is_reference_to_classx
-{
-    BOOST_STATIC_CONSTANT(
-        bool, value
-        = (boost::type_traits::ice_and<
-           is_reference<T>::value
-           , is_class<
-                typename remove_cv<
-                    typename remove_reference<T>::type
-                >::type
-             >::value
-         >::value)
-        );
-    typedef mpl::bool_<value> type;
-    BOOST_MPL_AUX_LAMBDA_SUPPORT(1,is_reference_to_class,(T))
-};
-
-template <class T>
 struct is_reference_to_class
     : mpl::and_<
           is_reference<T>
