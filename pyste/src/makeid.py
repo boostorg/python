293978d13dcd555fb4b4b4d5074aa25aeb02be17
nobody <nobody@localhost>
2003-03-22 13:34:53 +0000

    This commit was manufactured by cvs2svn to create tag 'merged_to_1_30_0'.
    [SVN r18045]

--- pyste/src/makeid.py
+++
@@ -1,9 +0,0 @@
-
-def makeid(name):
-    'Returns the name as a valid identifier'
-    for invalidchar in ('::', '<', '>', ' ', ','):
-        name = name.replace(invalidchar, '_') 
-    # avoid duplications of '_' chars
-    names = [x for x in name.split('_') if x]
-    return '_'.join(names)
-
