8c6f266370a78943626fd68c926ce09e408947eb
nobody <nobody@localhost>
2003-05-30 21:52:34 +0000

    This commit was manufactured by cvs2svn to create tag 'version_0-8-1'.
    [SVN r18620]

--- pyste/tests/nt_all.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call nt_build_all.bat
-runtests.py
-call nt_clean.bat
-call nt_build_all.bat --multiple
-runtests.py
-call nt_clean.bat 
--- pyste/tests/nt_build_pyste.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set BOOST_INCLUDE=D:\Programming\Libraries\boost-cvs\boost
-set out=_%1.cpp
-if "%2" == "--multiple" set out=_%1
-rem python ../src/pyste.py %2 -I%BOOST_INCLUDE -I../example --module=%1 --out=%out ../example/%1.pyste  
-pyste %2 -I%BOOST_INCLUDE -I../example --module=%1 --out=%out ../example/%1.pyste   
-
-scons --quiet multiple=%2 %1.dll
