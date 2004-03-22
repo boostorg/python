486fff38c2b8dc6cfa965014e75acc38bfb265b7
Jonathan Brandmeyer <jbrandmeyer@earthlink.net>
2004-03-22 01:57:09 +0000

    Merge changes from HEAD to: Throw an IndexError when given an extended slice; Clip bounds of slices in a mannar consistant with builtin containers; Prevent undefined behavior within the STL when given certain kinds of empty slices.
    [SVN r22539]

--- include/boost/python/suite/indexing/detail/indexing_suite_detail.hpp
+++ include/boost/python/suite/indexing/detail/indexing_suite_detail.hpp
@@ -570,15 +570,40 @@ namespace boost { namespace python { namespace detail {
         base_get_slice_data(
             Container& container, PySliceObject* slice, Index& from, Index& to)
         {
+            if (Py_None != slice->step) {
+                PyErr_SetString( PyExc_IndexError, "slice step size not supported.");
+                throw_error_already_set();
+            }
+
+            Index min_index = DerivedPolicies::get_min_index(container);
+            Index max_index = DerivedPolicies::get_max_index(container);
+            
+            
             if (Py_None == slice->start)
-                from = DerivedPolicies::get_min_index(container);
-            else 
-                from = DerivedPolicies::convert_index(container, slice->start);
+                from = min_index;
+            else {
+                from = extract<long>( slice->start);
+                if (from < 0) // Negative slice index
+                    from += max_index;
+                if (from < 0) // Clip lower bounds to zero
+                    from = 0;
+                if (from > max_index) // Clip upper bounds to max_index.
+                    from = max_index;
+                
+            }
 
             if (Py_None == slice->stop)
                 to = DerivedPolicies::get_max_index(container);
-            else 
-                to = DerivedPolicies::convert_index(container, slice->stop);
+            else {
+                to = extract<long>( slice->stop);
+                if (to < 0)
+                    to += max_index;
+                if (to < 0)
+                    to = 0;
+                if (to > max_index)
+                    to = max_index;
+            }
+		
         }        
    
         static void 
