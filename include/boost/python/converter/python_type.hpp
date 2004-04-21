ef802e16911e23583ebe1e1cebbd338d09ae10e9
Nikolay Mladenov <nikolay.mladenov@gmail.com>
2004-04-21 18:49:06 +0000

    fixed name collision
    [SVN r22682]

--- include/boost/python/converter/python_type.hpp
+++ include/boost/python/converter/python_type.hpp
@@ -5,16 +5,23 @@
 
 namespace boost {namespace python {namespace converter{ 
 namespace detail{
-    inline void * identity (PyObject *p){return p;}
     namespace strip_type_info{
         BOOST_PYTHON_DECL void insert(boost::python::type_info, boost::python::type_info);
         BOOST_PYTHON_DECL type_info query(boost::python::type_info);
     }
+
+    class python_class_base : public PyObject{
+        python_class_base ();
+    public:
+        static void *converter (PyObject *p){return p;}
+    };
+
+    
         
 }// namespace converter::detail
 
 
-template <class T> struct python_class : PyObject
+template <class T> struct python_class : detail::python_class_base
 {
     typedef python_class<T> this_type;
     typedef T type;
@@ -24,14 +31,12 @@ template <class T> struct python_class : PyObject
 
         if ( !first_time ) return;
         first_time = false;
-        converter::registry::insert(&detail::identity, boost::python::type_id<this_type>());
+        converter::registry::insert(&detail::python_class_base::converter, boost::python::type_id<this_type>());
         converter::detail::strip_type_info::insert(boost::python::type_id<type>(), boost::python::type_id<type*>());
         converter::detail::strip_type_info::insert(boost::python::type_id<type>(), boost::python::type_id<type const *>());
         converter::detail::strip_type_info::insert(boost::python::type_id<type>(), boost::python::type_id<this_type*>());
         converter::detail::strip_type_info::insert(type_id<type>(), type_id<shared_ptr<type> >());
     }
-private:
-    python_class();
 };
 
         
