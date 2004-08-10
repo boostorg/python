9ea7f100cca598cd8fe910c3b3773d51871fd105
Ralf W. Grosse-Kunstleve <rwgk@yahoo.com>
2004-08-10 17:17:52 +0000

    Add license, copyright.
    [SVN r24392]

--- example/getting_started1.cpp
+++ example/getting_started1.cpp
@@ -1,4 +1,6 @@
-// Example by Ralf W. Grosse-Kunstleve
+// Copyright Ralf W. Grosse-Kunstleve 2002-2004. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <string>
 
