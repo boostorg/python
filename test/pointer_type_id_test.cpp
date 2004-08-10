ecae47236e7bcf965fd1e88841da7dda508f70c6
Dave Abrahams <dave@boostpro.com>
2004-08-10 14:59:58 +0000

    Add license, copyright.
    [SVN r24380]

--- test/pointer_type_id_test.cpp
+++ test/pointer_type_id_test.cpp
@@ -1,3 +1,6 @@
+// Copyright David Abrahams 2004. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/python/type_id.hpp>
 #include <cassert>
 #include <boost/python/converter/pointer_type_id.hpp>
