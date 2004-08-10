cb45ee8879ab91f684103aed6e7a4b1f71a79997
Dave Abrahams <dave@boostpro.com>
2004-08-10 16:14:51 +0000

    Add license, copyright.
    [SVN r24386]

--- test/defaults.py
+++ test/defaults.py
@@ -1,3 +1,6 @@
+# Copyright David Abrahams 2004. Distributed under the Boost
+# Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 """
 >>> False = 0 # Python 2.2 needs these
 >>> True = 1
--- test/multi_arg_constructor.py
+++ test/multi_arg_constructor.py
@@ -1,3 +1,6 @@
+# Copyright David Abrahams 2004. Distributed under the Boost
+# Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 '''
 >>> from multi_arg_constructor_ext import *
 >>> a = A(1.0, 2, 3, 4, 5, 6, 7.0, 8.1, 9.3)
