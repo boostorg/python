77c2c8d77ce3b5178a14247c14584ba893761a46
Joel de Guzman <djowel@gmail.com>
2004-08-11 02:21:27 +0000

    Copyright and License
    [SVN r24395]

--- test/vector_indexing_suite.cpp
+++ test/vector_indexing_suite.cpp
@@ -1,3 +1,7 @@
+// Copyright Joel de Guzman 2004. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include <boost/python/module.hpp>
 #include <boost/python/def.hpp>
