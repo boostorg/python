c5cf576deb4737cc55b1570feda321db09d9d572
Ralf W. Grosse-Kunstleve <rwgk@yahoo.com>
2004-09-06 18:42:41 +0000

    _DEBUG/BOOST_DEBUG_PYTHON/DEBUG_UNDEFINED_FROM_WRAP_PYTHON_H must appear before #include <pyconfig.h>
    [SVN r24942]

--- include/boost/python/detail/prefix.hpp
+++ include/boost/python/detail/prefix.hpp
@@ -10,11 +10,6 @@
 // Unfortunately, Boost.Python needs to #include <limits.h> first, at
 // least... but this gets us as close as possible.
 
-# include <pyconfig.h>
-# if defined(_SGI_COMPILER_VERSION) && _SGI_COMPILER_VERSION == 741
-#  undef _POSIX_C_SOURCE
-#  undef _XOPEN_SOURCE
-# endif
 # include <boost/python/detail/wrap_python.hpp>
 # include <boost/python/detail/config.hpp>
 
--- include/boost/python/detail/wrap_python.hpp
+++ include/boost/python/detail/wrap_python.hpp
@@ -20,6 +20,19 @@
 // 04 Mar 01  Rolled in some changes from the Dragon fork (Dave Abrahams)
 // 01 Mar 01  define PyObject_INIT() for Python 1.x (Dave Abrahams)
 
+#ifdef _DEBUG
+# ifndef BOOST_DEBUG_PYTHON
+#  undef _DEBUG // Don't let Python force the debug library just because we're debugging.
+#  define DEBUG_UNDEFINED_FROM_WRAP_PYTHON_H
+# endif
+#endif
+
+# include <pyconfig.h>
+# if defined(_SGI_COMPILER_VERSION) && _SGI_COMPILER_VERSION == 741
+#  undef _POSIX_C_SOURCE
+#  undef _XOPEN_SOURCE
+# endif
+
 //
 // Python's LongObject.h helpfully #defines ULONGLONG_MAX for us,
 // which confuses Boost's config
@@ -42,13 +55,6 @@
 
 #if PY_MAJOR_VERSION<2 || PY_MAJOR_VERSION==2 && PY_MINOR_VERSION<2
 #error Python 2.2 or higher is required for this version of Boost.Python.
-#endif
-
-#ifdef _DEBUG
-# ifndef BOOST_DEBUG_PYTHON
-#  undef _DEBUG // Don't let Python force the debug library just because we're debugging.
-#  define DEBUG_UNDEFINED_FROM_WRAP_PYTHON_H
-# endif
 #endif
 
 //
