7eb9f910ba1886297126d1d6373ccde812975f00
Dave Abrahams <dave@boostpro.com>
2004-09-16 00:47:59 +0000

    moved indirect_traits to boost/detail
    [SVN r25127]

--- include/boost/python/detail/decorated_type_id.hpp
+++ include/boost/python/detail/decorated_type_id.hpp
@@ -37,9 +37,9 @@ inline decorated_type_info decorated_type_id(boost::type<T>* = 0)
     return decorated_type_info(
         type_id<T>()
         , decorated_type_info::decoration(
-            (is_const<T>::value || python::detail::is_reference_to_const<T>::value
+            (is_const<T>::value || indirect_traits::is_reference_to_const<T>::value
              ? decorated_type_info::const_ : 0)
-            | (is_volatile<T>::value || python::detail::is_reference_to_volatile<T>::value
+            | (is_volatile<T>::value || indirect_traits::is_reference_to_volatile<T>::value
                ? decorated_type_info::volatile_ : 0)
             | (is_reference<T>::value ? decorated_type_info::reference : 0)
             )
