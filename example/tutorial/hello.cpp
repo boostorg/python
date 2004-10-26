59b596e1eec8cdb8626c92092d28e301384136d5
Joel de Guzman <djowel@gmail.com>
2004-10-26 00:13:19 +0000

    license
    [SVN r25867]

--- example/tutorial/hello.cpp
+++ example/tutorial/hello.cpp
@@ -1,3 +1,6 @@
+//  Copyright Joel de Guzman 2002-2004. Distributed under the Boost
+//  Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt 
+//  or copy at http://www.boost.org/LICENSE_1_0.txt)
 //  Hello World Example from the tutorial
 //  [Joel de Guzman 10/9/2002]
 
