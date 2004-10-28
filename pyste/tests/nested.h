c99bd3ab006e26663441ccabcbaa2fc8ea61bffb
John Maddock <john@johnmaddock.co.uk>
2004-10-28 11:06:12 +0000

    Merged added copyrights from main branch.
    [SVN r25913]

--- pyste/tests/nested.h
+++ pyste/tests/nested.h
@@ -1,3 +1,9 @@
+/* Copyright Bruno da Silva de Oliveira 2003. Use, modification and 
+ distribution is subject to the Boost Software License, Version 1.0.
+ (See accompanying file LICENSE_1_0.txt or copy at 
+ http:#www.boost.org/LICENSE_1_0.txt) 
+ */ 
+
 #ifndef NESTED_H
 #define NESTED_H
 
