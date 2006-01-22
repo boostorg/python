8eba0eb25ba4d1a3362ff1ab3fe69f403ff18bee
Ralf W. Grosse-Kunstleve <rwgk@yahoo.com>
2006-01-22 19:21:08 +0000

    initialize all slots of PyTypeObject to avoid g++ -Wall -W warnings
    [SVN r32370]

--- include/boost/python/opaque_pointer_converter.hpp
+++ include/boost/python/opaque_pointer_converter.hpp
@@ -109,7 +109,50 @@ PyTypeObject opaque_pointer_converter<Pointer>::type_object =
     0,
     sizeof(typename opaque_pointer_converter<Pointer>::instance),
     0,
-    ::boost::python::detail::dealloc
+    ::boost::python::detail::dealloc,
+    0,          /* tp_print */
+    0,          /* tp_getattr */
+    0,          /* tp_setattr */
+    0,          /* tp_compare */
+    0,          /* tp_repr */
+    0,          /* tp_as_number */
+    0,          /* tp_as_sequence */
+    0,          /* tp_as_mapping */
+    0,          /* tp_hash */
+    0,          /* tp_call */
+    0,          /* tp_str */
+    0,          /* tp_getattro */
+    0,          /* tp_setattro */
+    0,          /* tp_as_buffer */
+    0,          /* tp_flags */
+    0,          /* tp_doc */
+    0,          /* tp_traverse */
+    0,          /* tp_clear */
+    0,          /* tp_richcompare */
+    0,          /* tp_weaklistoffset */
+    0,          /* tp_iter */
+    0,          /* tp_iternext */
+    0,          /* tp_methods */
+    0,          /* tp_members */
+    0,          /* tp_getset */
+    0,          /* tp_base */
+    0,          /* tp_dict */
+    0,          /* tp_descr_get */
+    0,          /* tp_descr_set */
+    0,          /* tp_dictoffset */
+    0,          /* tp_init */
+    0,          /* tp_alloc */
+    0,          /* tp_new */
+    0,          /* tp_free */
+    0,          /* tp_is_gc */
+    0,          /* tp_bases */
+    0,          /* tp_mro */
+    0,          /* tp_cache */
+    0,          /* tp_subclasses */
+    0,          /* tp_weaklist */
+#if PYTHON_API_VERSION >= 1012
+    0           /* tp_del */
+#endif
 };
 }} // namespace boost::python
 #  ifdef BOOST_MSVC
