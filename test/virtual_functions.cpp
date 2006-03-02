28ef4a9e3808356d2c85e4b6ae25540108c0cccb
Markus Schöpflin <markus.schoepflin@comsoft.de>
2006-03-02 17:12:18 +0000

    Reorder includes to make sure that python headers are included first, this makes the test pass on hp_cxx_71_006_tru64.
    [SVN r33202]

--- test/virtual_functions.cpp
+++ test/virtual_functions.cpp
@@ -2,7 +2,6 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-#include <boost/assert.hpp>
 
 #include <boost/python/class.hpp>
 #include <boost/python/module.hpp>
@@ -11,6 +10,8 @@
 #include <boost/python/call_method.hpp>
 #include <boost/ref.hpp>
 #include <boost/utility.hpp>
+
+#include <boost/assert.hpp>
 
 using namespace boost::python;
 
