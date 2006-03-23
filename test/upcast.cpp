3fdfb30e334935ae1f871ff3b603f395a75d95bf
Markus Schöpflin <markus.schoepflin@comsoft.de>
2006-03-23 09:38:03 +0000

    Include python first, fixes error on Tru64/CXX.
    [SVN r33454]

--- test/upcast.cpp
+++ test/upcast.cpp
@@ -2,8 +2,9 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-#include <boost/detail/lightweight_test.hpp>
+
 #include <boost/python/cast.hpp>
+#include <boost/detail/lightweight_test.hpp>
 
 struct X { long x; };
 struct Y : X, PyObject {};
@@ -16,4 +17,3 @@ int main()
     BOOST_TEST(&boost::python::upcast<PyObject>(&y)->ob_refcnt == &y.ob_refcnt);
     return boost::report_errors();
 }
-
