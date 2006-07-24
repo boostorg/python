8fb6e1f48d37f56f7e76f0aa152176f5c9297baa
Gennaro Prota <gennaro.prota@gmail.com>
2006-07-24 22:28:41 +0000

    (merge from head) removed tabs (inspect tool)
    [SVN r34723]

--- test/exec.cpp
+++ test/exec.cpp
@@ -54,7 +54,7 @@ void exec_test()
   // Register the module with the interpreter
   if (PyImport_AppendInittab("embedded_hello", initembedded_hello) == -1)
     throw std::runtime_error("Failed to add embedded_hello to the interpreter's "
-			     "builtin modules");
+                 "builtin modules");
   // Retrieve the main module
   python::object main = python::import("__main__");
   
