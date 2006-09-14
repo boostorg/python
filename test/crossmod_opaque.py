864ece5539b27ee3a29c3518afa1bb9c8b512ca0
Gottfried Ganßauge <gottfried.ganssauge@haufe.de>
2006-09-14 19:06:33 +0000

    cross module compatibility test for opaque
    [SVN r35111]

---
+++ test/crossmod_opaque.py
@@ -0,0 +1,14 @@
+# Copyright Gottfried Ganﬂauge 2006.
+# Distributed under the Boost Software License, Version 1.0. (See
+# accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+print "running..."
+
+import crossmod_opaque_a
+import crossmod_opaque_b
+
+crossmod_opaque_a.get()
+crossmod_opaque_b.get()
+
+print "Done."
