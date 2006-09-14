777ce7b561cdcb5c3adf3960505ee512e1e25d30
Ralf W. Grosse-Kunstleve <rwgk@yahoo.com>
2006-09-14 21:53:00 +0000

    magic coding: iso-latin1 comment added to avoid Python SyntaxError
    [SVN r35113]

--- test/crossmod_opaque.py
+++ test/crossmod_opaque.py
@@ -1,3 +1,4 @@
+# -*- coding: iso-latin-1 -*-
 # Copyright Gottfried Ganﬂauge 2006.
 # Distributed under the Boost Software License, Version 1.0. (See
 # accompanying file LICENSE_1_0.txt or copy at
