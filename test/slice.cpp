029618f7199af0062b8001134dc3fa53deaeead5
Dave Abrahams <dave@boostpro.com>
2006-09-18 18:25:57 +0000

    merged from HEAD
    [SVN r35165]

--- test/slice.cpp
+++ test/slice.cpp
@@ -49,7 +49,7 @@ bool check_string_rich_slice()
 // value doesn't make any sense.
 #define ASSERT_EQUAL( e1, e2 ) \
 if ((e1) != (e2)) \
-    return str("assertion failed: " #e1 " == " #e2 "\nLHS:\n") /*+ str(e1) + "\nRHS:\n" + str(e2)*/; \
+    return object("assertion failed: " #e1 " == " #e2 "\nLHS:\n") /*+ str(e1) + "\nRHS:\n" + str(e2)*/; \
 else
 
 // These tests work with Python 2.2, but you must have Numeric installed.
