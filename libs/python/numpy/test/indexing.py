88dd5330d05ddbfc1e5d6f933ede355004935897
Stefan Seefeld <stefan@seefeld.name>
2011-06-27 14:07:54 +0000

    Tidy indexing tests.

--- libs/python/numpy/test/indexing.py
+++ libs/python/numpy/test/indexing.py
@@ -4,24 +4,24 @@ import indexing_mod
 
 class TestIndexing(unittest.TestCase):
 
-	def testSingle(self):
-		x = numpy.arange(0,10)
-		for i in range(0,10):
-			indexing_mod.single(x,i,i)
-		for i in range(-10,0):
-			indexing_mod.single(x,i,10+i)
+    def testSingle(self):
+        x = numpy.arange(0,10)
+	for i in range(0,10):
+            numpy.testing.assert_equal(indexing_mod.single(x,i), i)
+	for i in range(-10,0):
+            numpy.testing.assert_equal(indexing_mod.single(x,i),10+i)
 
-	def testSlice(self):
-		x = numpy.arange(0,10)
-		sl = slice(3,8)
-		b = [3,4,5,6,7]
-		indexing_mod.slice(x,sl,b)
+    def testSlice(self):
+        x = numpy.arange(0,10)
+	sl = slice(3,8)
+	b = [3,4,5,6,7]
+	numpy.testing.assert_equal(indexing_mod.slice(x,sl), b)
 
-	def testStepSlice(self):
-		x = numpy.arange(0,10)
-		sl = slice(3,8,2)
-		b = [3,5,7]
-		indexing_mod.step_slice(x,sl,b)
+    def testStepSlice(self):
+        x = numpy.arange(0,10)
+        sl = slice(3,8,2)
+        b = [3,5,7]
+        numpy.testing.assert_equal(indexing_mod.slice(x,sl), b)
 
 if __name__=="__main__":
-	unittest.main()
+    unittest.main()
--- libs/python/numpy/test/indexing_mod.cpp
+++ libs/python/numpy/test/indexing_mod.cpp
@@ -1,48 +1,14 @@
 #include <boost/python/numpy.hpp>
-#include <assert.h>
 #include <boost/python/slice.hpp>
 
 namespace bp = boost::python;
 
-void single(bp::numpy::ndarray ndarr, int i,bp::object value) {
-	bp::object element = bp::extract<bp::object>(ndarr[i]);
-	assert(element == value);
+bp::object single(bp::numpy::ndarray ndarr, int i) { return ndarr[i];}
+bp::object slice(bp::numpy::ndarray ndarr, bp::slice sl) { return ndarr[sl];}
+
+BOOST_PYTHON_MODULE(indexing_mod) 
+{
+  bp::numpy::initialize();
+  bp::def("single", single);
+  bp::def("slice", slice);
 }
-
-
-void slice(bp::numpy::ndarray ndarr, bp::slice sl,bp::object val) {
-	int start = bp::extract<int>(sl.start());
-	int stop  = bp::extract<int>(sl.stop());
-	unsigned j=0;
- 	for (int i = start; i < stop; i++)
-    	{
-			bp::object element = bp::extract<bp::object>(ndarr[i]);
-			bp::object value = bp::extract<bp::object>(val[j]);
-			assert(element == value);
-			++j;
-	}
-}
-
-
-void step_slice(bp::numpy::ndarray ndarr, bp::slice sl,bp::object val) {
-	int start = bp::extract<int>(sl.start());
-	int stop  = bp::extract<int>(sl.stop());
-	int step = bp::extract<int>(sl.step());
-	unsigned j=0;
- 	for (int i = start; i < stop; i=i+step)
-    	{
-			bp::object element = bp::extract<bp::object>(ndarr[i]);
-			bp::object value = bp::extract<bp::object>(val[j]);
-			assert(element == value);
-			++j;
-	}
-}
-
-
-BOOST_PYTHON_MODULE(indexing_mod) {
-    bp::numpy::initialize();
-	bp::def("single",&single);
-	bp::def("slice",&slice);
-	bp::def("step_slice",&step_slice);
-}
-
