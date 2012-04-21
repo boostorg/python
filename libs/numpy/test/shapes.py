dca44829a681fa8cebf541aa97e82907ae6f14ec
Jim Bosch <jbosch@astro.princeton.edu>
2012-04-21 16:34:01 -0400

    untabify python test files

--- libs/numpy/test/shapes.py
+++ libs/numpy/test/shapes.py
@@ -5,12 +5,12 @@ import unittest
 import numpy
 
 class TestShapes(unittest.TestCase):
-	
-	def testShapes(self):
-		a1 = numpy.array([(0,1),(2,3)])
-		a1_shape = (1,4)
-		a1 = shapes_mod.reshape(a1,a1_shape)
-		self.assertEqual(a1_shape,a1.shape)
+    
+    def testShapes(self):
+        a1 = numpy.array([(0,1),(2,3)])
+        a1_shape = (1,4)
+        a1 = shapes_mod.reshape(a1,a1_shape)
+        self.assertEqual(a1_shape,a1.shape)
 
 if __name__=="__main__":
-	unittest.main()
+    unittest.main()
