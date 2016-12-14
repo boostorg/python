7178a70176fe68cb7e470ca53ab47075e0106a70
Tadeu Manoel <tadeu@users.noreply.github.com>
2016-12-14 10:54:37 -0200

    Fix a problem where test_builtin_converters.py is not being run (#94)
    This was happening because the module docstring was not the first statement.

--- test/test_builtin_converters.py
+++ test/test_builtin_converters.py
@@ -1,9 +1,6 @@
 # Copyright David Abrahams 2004. Distributed under the Boost
 # Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-import sys
-if (sys.version_info.major >= 3):
-    long = int
 r"""
 >>> from builtin_converters_ext import *
 
@@ -282,6 +279,10 @@ Check that classic classes also work
 
 >>> assert return_null_handle() is None
 """
+
+import sys
+if (sys.version_info.major >= 3):
+    long = int
 
 def run(args = None):
     import sys
