b1dbe110ab6c3aba32c314b39ed33710ad7a563e
Bryn Lloyd <lloyd@itis.swiss>
2022-01-03 15:27:13 +0100

    add optional compile time check

--- include/boost/python/make_function.hpp
+++ include/boost/python/make_function.hpp
@@ -56,7 +56,13 @@ namespace detail
       typedef typename detail::error::more_keywords_than_function_arguments<
           NumKeywords::value, arity
           >::too_many_keywords assertion BOOST_ATTRIBUTE_UNUSED;
-    
+
+#ifdef BOOST_PYTHON_CHECK_LESS_KEYWORDS_THAN_ARITY
+      typedef typename detail::error::less_keywords_than_function_arguments<
+          NumKeywords::value, arity
+      >::too_few_keywords assertion BOOST_ATTRIBUTE_UNUSED;
+#endif
+
       return objects::function_object(
           detail::caller<F,CallPolicies,Sig>(f, p)
         , kw);
