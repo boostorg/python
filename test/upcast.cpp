7fd39323acb89378882f6188ca2d3f63de199c08
Stefan Seefeld <stefan@seefeld.name>
2025-11-01 22:30:52 -0400

    Don't rely on Py_REFCNT to test upcast.

--- test/upcast.cpp
+++ test/upcast.cpp
@@ -13,7 +13,7 @@ int main()
 {
     PyTypeObject o;
     Y y;
-    BOOST_TEST(&Py_REFCNT(boost::python::upcast<PyObject>(&o)) == &Py_REFCNT(&o));
-    BOOST_TEST(&Py_REFCNT(boost::python::upcast<PyObject>(&y)) == &Py_REFCNT(&y));
+    BOOST_TEST(boost::python::upcast<PyObject>(&o) == reinterpret_cast<PyObject*>(&o));
+    BOOST_TEST(boost::python::upcast<PyObject>(&y) == &y);
     return boost::report_errors();
 }
